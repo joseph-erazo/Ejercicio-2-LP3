10eb86b20ed44f466f15c5936a285ab4a8784ea2
joseph-erazo <abrahamponce.n111@gmail.com>
2022-02-18 20:27:32 -0800

    Ejercicio #2 (Terminado)
    Aquí está todo lo referente al ejercicio asignado #2.

---
+++ Ejercicio-2-LP3/Ejercicio-2-LP3/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Ejercicio_2_LP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EdadTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.TerminarButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar nombre:";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(160, 8);
+            this.NombreTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(148, 26);
+            this.NombreTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresar edad:";
+            // 
+            // EdadTextBox
+            // 
+            this.EdadTextBox.Location = new System.Drawing.Point(160, 57);
+            this.EdadTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.EdadTextBox.Name = "EdadTextBox";
+            this.EdadTextBox.Size = new System.Drawing.Size(148, 26);
+            this.EdadTextBox.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 118);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(248, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.Location = new System.Drawing.Point(80, 237);
+            this.AgregarButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(112, 35);
+            this.AgregarButton.TabIndex = 5;
+            this.AgregarButton.Text = "Agregar";
+            this.AgregarButton.UseVisualStyleBackColor = true;
+            this.AgregarButton.Click += new System.EventHandler(this.AgregarButton_Click);
+            // 
+            // TerminarButton
+            // 
+            this.TerminarButton.Location = new System.Drawing.Point(307, 237);
+            this.TerminarButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TerminarButton.Name = "TerminarButton";
+            this.TerminarButton.Size = new System.Drawing.Size(112, 35);
+            this.TerminarButton.TabIndex = 6;
+            this.TerminarButton.Text = "Terminar";
+            this.TerminarButton.UseVisualStyleBackColor = true;
+            this.TerminarButton.Click += new System.EventHandler(this.TerminarButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TerminarButton);
+            this.Controls.Add(this.AgregarButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.EdadTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EdadTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.Button TerminarButton;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ejercicio-2-LP3/Ejercicio-2-LP3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2_LP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AgregarButton_Click(object sender, EventArgs e)
+        {
+
+            string[] nombre = new string[1];
+            string[] edad = new string[1];
+            string[] nombre_edad = new string[1];
+
+            for (int i = 0; i < 1; i++)
+            {
+                nombre[i] = NombreTextBox.Text;
+                edad[i] = EdadTextBox.Text;
+                nombre_edad[i] = $"{nombre[i]}, {edad[i]} años de edad.";
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                comboBox1.Items.Add(nombre_edad[i]);
+            }
+
+            NombreTextBox.Clear();
+            EdadTextBox.Clear();
+        }
+
+        private void TerminarButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
